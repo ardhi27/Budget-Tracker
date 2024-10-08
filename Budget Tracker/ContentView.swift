35d06d37a2c2ffe6ba0fa122942e36207d6e3bcb
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-10-08 21:52:07 +0800

    Add remote

--- Budget Tracker/ContentView.swift
+++ Budget Tracker/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var dailyBudget = 30000
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(.black)
+            VStack {
+                ZStack{
+                    Color("Custom Gray Color").opacity(0.5)
+                    VStack{
+                        HStack{
+                            Text("Budget Today: ").bold()
+                            Text("Rp\(dailyBudget)")
+                            Spacer()
+                        }.padding([.leading, .top], 30).foregroundColor(.white)
+                        Spacer()
+                        HStack{
+                            Text("A**** K******* Ardhiyanda").bold()
+                            Spacer()
+                            HStack(spacing: -10){
+                                Circle().fill(Color("Custom Red")).frame(height: 40)
+                                Circle().fill(Color("Custom Yellow")).frame(height: 40)
+                            }
+                        }.padding([.leading, .trailing, .bottom], 30).foregroundColor(.white)
+                    }
+                }.frame(height: 200).cornerRadius(15)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct DailyBudgetModal: View{
+    var body: some View{
+        Text("Hello, world!")
     }
 }
 
