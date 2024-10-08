6df168221d461868f000b188d21676126fdae797
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-10-08 21:02:59 +0800

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Agung Ardhiyanda on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
