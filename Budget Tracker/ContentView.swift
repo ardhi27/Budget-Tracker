846d841c2bf6946a8766c4eb45cba22ca307c5d5
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-10-09 15:52:29 +0800

    Feat: add new element for budget output

--- Budget Tracker/ContentView.swift
+++ Budget Tracker/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var dailyBudget = 30000
+    @State var remainingBudget = 100000
     var body: some View {
         ZStack{
             Color(.black)
@@ -16,23 +17,34 @@ struct ContentView: View {
                 ZStack{
                     Color("Custom Gray Color").opacity(0.5)
                     VStack{
-                        HStack{
-                            Text("Budget Today: ").bold()
-                            Text("Rp\(dailyBudget)")
+                        VStack{
+                            HStack{
+                                Text("Budget Today: ").bold()
+                                Text("Rp\(dailyBudget)")
+                                Spacer()
+                            }.padding([.leading, .top], 30).foregroundColor(.white)
                             Spacer()
-                        }.padding([.leading, .top], 30).foregroundColor(.white)
-                        Spacer()
-                        HStack{
-                            Text("A**** K******* Ardhiyanda").bold()
-                            Spacer()
-                            HStack(spacing: -10){
-                                Circle().fill(Color("Custom Red")).frame(height: 40)
-                                Circle().fill(Color("Custom Yellow")).frame(height: 40)
-                            }
-                        }.padding([.leading, .trailing, .bottom], 30).foregroundColor(.white)
+                            HStack{
+                                Text("A**** K******* Ardhiyanda").bold()
+                                Spacer()
+                                HStack(spacing: -10){
+                                    Circle().fill(Color("Custom Red")).frame(height: 40)
+                                    Circle().fill(Color("Custom Yellow")).frame(height: 40)
+                                }
+                            }.padding([.leading, .trailing, .bottom], 30).foregroundColor(.white)
+                        }
                     }
                 }.frame(height: 200).cornerRadius(15)
+                ZStack{
+                    Color("Custom Gray Color").opacity(0.5)
+                    HStack{
+                        Text("Budget Output Today:")
+                        Spacer()
+                        Text("Rp\(remainingBudget)")
+                    }.padding([.leading, .trailing], 30)
+                }.frame(height: 100).cornerRadius(15).foregroundColor(.white)
                 Spacer()
+                Text("Button place").foregroundColor(.white)
             }
             .padding()
         }
